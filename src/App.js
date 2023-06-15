4cb1212cfb6fb83bffe2b7637b24fc0a5ca99710
Mushtaqmo <mushtaqmushtaq123@outlook.com>
2023-06-15 14:48:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import './index.css';
 
-function App() {
+const App = () => {
+  
+  const [products, setProducts] = useState([]);
+
+  
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
+  
+  
+  useEffect(() => {
+    const fetchMakeupProducts = async () => {
+      try {
+       
+        let response = await fetch('http://makeup-api.herokuapp.com/api/v1/products.json?product_type=lipstick')
+        let products = await response.json()
+        setProducts(products)
+      } catch (error) {
+        console.error("Failed to fetch products:", error)
+      }
+    }
+
+    fetchMakeupProducts();
+  }, []);
+
+  const onChooseRandomProduct = () => {
+   
+    const randomIndex = Math.floor(Math.random() * products.length)
+    const selectedProduct = products[randomIndex]
+    setSelectedProduct(selectedProduct);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 id="mainHeader">Makeup API</h1>
+
+      <div>
+        <h2 id="secondHeader">Lipsticks</h2>
+
+        <div>
+        <img src="./images/lipstick1.jpg" />
+
+
+
+        </div>
+
+        {
+          
+          selectedProduct != null &&
+          <div className="lipsticks-container">
+           
+            <div className="lipstick-item">
+              <h3>ID: {selectedProduct.id}</h3>
+              <p>Name: {selectedProduct.name}</p>
+              <p>Brand: {selectedProduct.brand}</p>
+              <p>Price: {selectedProduct.price}</p>
+            </div>
+          </div>
+        }
+      </div>
+
+      <button onClick={onChooseRandomProduct}>Randomise Product</button>
     </div>
-  );
+  )
 }
 
 export default App;
